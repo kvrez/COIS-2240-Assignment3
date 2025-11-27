9ea91c627764ad9e14acef0b2ef9d4efee91debd
Kyle Perez <kylevperez@gmail.com>
2025-11-27 18:19:17 -0500

    Refactor Vehicle constructor with capitalize helper method

--- Vehicle.java
+++ Vehicle.java
@@ -8,16 +8,8 @@ public abstract class Vehicle {
     public enum VehicleStatus { Available, Held, Rented, UnderMaintenance, OutOfService }
 
     public Vehicle(String make, String model, int year) {
-    	if (make == null || make.isEmpty())
-    		this.make = null;
-    	else
-    		this.make = make.substring(0, 1).toUpperCase() + make.substring(1).toLowerCase();
-    	
-    	if (model == null || model.isEmpty())
-    		this.model = null;
-    	else
-    		this.model = model.substring(0, 1).toUpperCase() + model.substring(1).toLowerCase();
-    	
+        this.make = capitalize(make);
+        this.model = capitalize(model);
         this.year = year;
         this.status = VehicleStatus.Available;
         this.licensePlate = null;
@@ -25,6 +17,13 @@ public abstract class Vehicle {
 
     public Vehicle() {
         this(null, null, 0);
+    }
+    
+    private String capitalize(String input) {
+        if (input == null || input.isEmpty()) {
+            return null;
+        }
+        return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
     }
 
     public void setLicensePlate(String plate) {
